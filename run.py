d5925dbb9dc3e382db0fed2b2ea1b826c53dc8b2
RMoran98 <moranrachel359@gmail.com>
2023-10-11 13:47:25 +0100

    Create instructions and allow user input in get_sales function

--- run.py
+++ run.py
@@ -11,5 +11,14 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('adore_books')
 
-sales = SHEET.worksheet('sales')
-data = sales.get_all_values()
+
+def get_sales():
+    """
+    This function will request sales data from the most recent quarter from the user as comma separated values
+    """
+    print("Please enter the sales data from the last quarter.")
+    print("The data should be in order of genre, as follows: Fantasy, Romance, Thriller, Horror, Sci Fi.")
+    print("You should input the five numbers separated by a comma only, without any spaces.")
+    print("Example: 1,2,3,4,5\n")
+
+    sales_str = input("Please input sales data here: ")
